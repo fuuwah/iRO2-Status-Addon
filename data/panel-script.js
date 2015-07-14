5614ecf3f481d064ba6205ef479663a19bc51ce8
Gabriel Cavallo <gabrielcavallo@mail.com>
2015-07-14 00:39:26 -0400

    Use new API backend

--- data/panel-script.js
+++ data/panel-script.js
@@ -1,9 +1,15 @@
 function refresh() {
 	$.ajax({
 		type: "POST",
+		dataType: "json",
 		url: "http://status.niceboat-guild.com/",
 		success: function(data) {
-			$("#iRO2").html($(data).find("#iRO2"));
+			$("#iRO2").empty();
+			for (var d in data) {
+				$("#iRO2").append(
+					'<li class="' + data[d].Name + ' ' + data[d].Status + '">' +
+					data[d].Name + ' is <strong>' + data[d].Status + '</strong>.</li>');
+			}
 		},
 		complete: function() {
 			setTimeout(refresh, 10000);
