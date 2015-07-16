54bdb980e269a2d195dbc872b7aaff9928602dd9
Gabriel Cavallo <gabrielcavallo@mail.com>
2015-07-16 02:37:53 -0400

    Use DOM appendChild instead of jQuery append

--- data/panel-script.js
+++ data/panel-script.js
@@ -6,9 +6,18 @@ function refresh() {
 		success: function(data) {
 			$("#iRO2").empty();
 			for (var d in data) {
-				$("#iRO2").append(
-					'<li class="' + data[d].Name + ' ' + data[d].Status + '">' +
-					data[d].Name + ' is <strong>' + data[d].Status + '</strong>.</li>');
+				var list_node = document.createElement('li');
+				list_node.id = data[d].Name;
+				list_node.className = data[d].Status;
+				var list_text = document.createTextNode(data[d].Name + ' is ');
+				list_node.appendChild(list_text);
+
+				var status_node = document.createElement('strong');
+				var status_text = document.createTextNode(data[d].Status);
+				status_node.appendChild(status_text);
+				list_node.appendChild(status_node);
+
+				document.getElementById("iRO2").appendChild(list_node);
 			}
 		},
 		complete: function() {
